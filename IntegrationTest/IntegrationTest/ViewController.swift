d4c4a21ed04f60b8cafac43ab7fc00424194d748
commit by Monica <monicadeswalios01@gmail.com>
2021-08-09 15:40:05 +0530

    Basic setup for integration test

---
+++ IntegrationTest/IntegrationTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntegrationTest
+//
+//  Created by Monica Mishipay on 09/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
